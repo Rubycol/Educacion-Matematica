ce5106e68cbb49f5f909d4a23d860887baa0ceae
Rubby Colonia <rubycolonia@gmail.com>
2021-07-17 01:31:45 -0500

    Initial Commit

---
+++ Educacion-Matematica/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Educacion-Matematica
+//
+//  Created by RUBBY on 17/07/21.
+//  Copyright © 2021 RUBBY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
